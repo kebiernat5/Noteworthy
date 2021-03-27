44ff4eafdc59f96f30c796d95246fbbd246d2b62
Kate Biernat <kebiernat5@gmail.com>
2021-03-26 21:59:26 -0400

    Working Code

---
+++ routes/apiroutes.js
@@ -0,0 +1,39 @@
+const router = require("express").Router();
+const fs = require("fs");
+const path = require("path");
+const dbPath = path.join(__dirname, "../db/db.json")
+
+
+router.get('/notes', (req, res) => {
+    fs.readFile(dbPath, "utf8", function(err, data){
+        res.json(JSON.parse(data))
+    })
+
+});
+
+router.post('/notes', (req, res) => {
+    const note = req.body;
+
+    fs.readFile(dbPath, "utf8", function(err, data){
+        const notes = (JSON.parse(data))
+        note.id = notes.length + "" + Math.random()
+        notes.push(note);
+        fs.writeFileSync("db/db.json", JSON.stringify(notes));
+        res.json(note);
+    })
+});
+
+router.post("/delete/:id", async (req, res) => {
+    const result = await Notification.findByIdAndDelete(req.note.id);
+    res.redirect("back");
+  });
+
+// async.createNewRecord(attributes);{
+//     attributes.id = this.generateUniqueID()
+// };
+// target note by id
+// /notes/:id
+// UUID npm package
+// Include a filter that returns the array that includes everything but that specific deleted ID
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,22 @@
+// Dependencies
+
+const express = require('express');
+const path = require('path');
+const apiroutes = require('./routes/apiroutes');
+const htmlroutes = require('./routes/htmlroutes')
+
+
+// Sets up the Express App
+
+const app = express();
+const PORT = 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(express.json());
+app.use("/api", apiroutes);
+app.use("/", htmlroutes);
+
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
